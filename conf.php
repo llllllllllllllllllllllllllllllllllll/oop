b5a27aa46955088e27e800e19d0ab261b9aa23dd
llllllllllllllllllllllllllllllllllll <aleksander.promvalds@khk.ee>
2018-01-19 09:50:30 +0200

    Projekti kaustadega seotud abikonstantid määratud

---
+++ conf.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: aleksander
+ * Date: 19.01.2018
+ * Time: 9:44
+ */
+// kaustade ja failide konstantsed nimetused
+define('MODEL_DIR', 'model/');
+define('VIEW_DIR', 'views/');
+define('CONTROL_DIR', 'controllers/');
