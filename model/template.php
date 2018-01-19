b398a241db74737ca446ed1b66ac512938fd4be6
llllllllllllllllllllllllllllllllllll <aleksander.promvalds@khk.ee>
2018-01-19 10:29:15 +0200

    Määratud template klassi muutujad

---
+++ model/template.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: aleksander
+ * Date: 19.01.2018
+ * Time: 10:22
+ */
+
+class template
+{
+    //klassi muutujad
+    var $file = ''; //HTML malli faili nimi
+    var $content = false; //HTML malli failist loetud sisu
+    var $vars = array();//HTML malli elementide ja reaalväärtuste paarid
+}
