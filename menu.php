4ec4981750253ced4686da68290c09adbd5d62c6
llllllllllllllllllllllllllllllllllll <aleksander.promvalds@khk.ee>
2018-01-23 11:06:41 +0200

    Menüü objekt ja menüü elemendi objekt loodud

---
+++ menu.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: aleksander
+ * Date: 23.01.2018
+ * Time: 11:03
+ */
+//loome menüü peamalli objekti template klassist
+$menuTmpl = new template('menu.menu');
+//loome menüü elemendi peamalli objekti template klassist
+$menuItemTmpl = new template('menu.menu_item');
