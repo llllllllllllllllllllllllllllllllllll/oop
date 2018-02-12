fb4655890d30dc663c70766e54e9f1e2e8e662c5
aleksander <aleksander.promvalds@khk.ee>
2018-02-12 09:35:14 +0200

    Loodi menüü koostamine andmebaasi andmetest

--- menu.php
+++ menu.php
@@ -6,32 +6,23 @@
  * Time: 11:03
  */
 //loome menüü peamalli objekti template klassist
-$menuTmpl = new template('menu.menu');
+$menu = new template('menu.menu');
 //loome menüü elemendi peamalli objekti template klassist
-$menuItemTmpl = new template('menu.menu_item');
+$menuItem = new template('menu.menu_item');
 
-//menüü reaalväärtused
-//avaleht
-$menuItemTmpl->set('menu_item_name', 'Avaleht');
-$link = $http->getLink();
-$menuItemTmpl->set('link', $link);
-$menuItem = $menuItemTmpl->parse();
-$menuTmpl->add('menu_items', $menuItem);
-
-//esimene
-$menuItemTmpl->set('menu_item_name', 'esimene');
-$link = $http->getLink(array('control' => 'esimene'));
-$menuItemTmpl->set('link', $link);
-$menuItem = $menuItemTmpl->parse();
-$menuTmpl->add('menu_items', $menuItem);
-
-//teine
-$menuItemTmpl->set('menu_item_name', 'teine');
-$link = $http->getLink(array('control' => 'teine'));
-$menuItemTmpl->set('link', $link);
-$menuItem = $menuItemTmpl->parse();
-$menuTmpl->add('menu_items', $menuItem);
-
-//täidame loodud elemendiga lehe menüü
-$menu = $menuTmpl->parse();
-$mainTmpl->set('menu', $menu);
+// koostame päringu menüü ja sisu ülesehitamiseks
+$sql = 'SELECT content_id, content, title '.
+        'FROM content WHERE parent_id='.fixDb(0).
+        ' AND show_in_menu='.fixDb(1);
+// küsime andmed andmebaasist
+$result = $db->getData($sql);
+// ehitame menüü
+if($result != false){
+        foreach ($result as $page){
+                $menuItem->set('menu_item_name', $page['title']);
+                $link = $http->getLink(array('page_id'=>$page['content_id']));
+                $menuItem->set('link', $link);
+                $menu->add('menu_items', $menuItem->parse());
+            }
+}
+$mainTmpl->add('menu', $menu->parse());
