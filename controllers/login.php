2bc6309a74709ae3ac1c63bdb544e6f227429cc1
aleksander <aleksander.promvalds@khk.ee>
2018-02-12 09:57:58 +0200

    Täiendati vormi väljund lingiga

--- controllers/login.php
+++ controllers/login.php
@@ -11,6 +11,11 @@ $loginForm = new template('login');
 $loginForm->set('kasutaja', 'Kasutajanimi');
 $loginForm->set('parool', 'Kasutaja parool');
 $loginForm->set('nupp', 'Logi sisse!');
+
+// loome link vorm töötluseks
+$link = $http->getLink(array('control'=>'login_do'));
+$loginForm->set('link', $link)
+
 // paneme väärtused malli
 // selleks oleks vaja trükida välja
 // sisse logimisvorm {content} elemendis
